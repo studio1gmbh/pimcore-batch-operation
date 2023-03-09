d2d38545aff05b04a29e24de5fa23cf2f48a5a4f
Lars77 <Lars77@users.noreply.github.com>
2023-03-09 08:27:32 +0000

    Apply php-cs-fixer changes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,8 +6,8 @@
  * This source file is available under following license:
  * - GNU General Public License v3.0 (GNU GPLv3)
  *
- * @copyright  Copyright (c) Studio1 Kommunikation GmbH (http://www.studio1.de)
- * @license    https://www.gnu.org/licenses/gpl-3.0.txt
+ *  @copyright  Copyright (c) Studio1 Kommunikation GmbH (http://www.studio1.de)
+ *  @license    https://www.gnu.org/licenses/gpl-3.0.txt
  */
 
 namespace Studio1\BatchOperationBundle\Controller;
